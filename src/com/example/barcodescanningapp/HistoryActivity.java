e419a49b687793dfd340184c59effc5e2d4353e3
Santosh reddy <rsantosh@iitrpr.ac.in>
2013-11-05 19:46:22 +0530

    actionbar

---
+++ src/com/example/barcodescanningapp/HistoryActivity.java
@@ -0,0 +1,118 @@
+package com.example.barcodescanningapp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HistoryActivity extends Activity {
+
+	/*// List view
+	private ListView lv;
+
+	// Listview Adapter
+	ArrayAdapter<String> adapter;
+
+	// Search EditText
+	EditText inputSearch;*/
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_multiplehistory);
+
+		/*List<String> history = new ArrayList<String>();
+		
+
+		Intent i = this.getIntent();
+		final String bus_stop_type = i.getExtras().getString("bus_stop_type");
+		final String bus_stop_source = i.getExtras().getString(
+				"bus_stop_source");
+		final String bus_stop_destination = i.getExtras().getString(
+				"bus_stop_destination");
+
+		lv = (ListView) findViewById(R.id.list_view);
+		//inputSearch = (EditText) findViewById(R.id.inputSearch);
+
+		// Adding items to listview
+		adapter = new ArrayAdapter<String>(this,
+				R.layout.activity_singlehistory, R.id.bus_stop_name,
+				history);
+		lv.setAdapter(adapter);
+
+		lv.setAdapter(adapter);*/
+
+/*		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, final View view,
+					int position, long id) {
+
+				String stop = ((TextView) view).getText().toString();
+				if (bus_stop_type.contains("source")) {
+					stop = bus_stop_type + '_' + stop + '_'
+							+ bus_stop_destination;
+				} else {
+					stop = bus_stop_type + '_' + bus_stop_source + '_' + stop;
+				}
+
+				
+				 * Context context = getApplicationContext();
+				 * 
+				 * CharSequence text = stop;
+				 * 
+				 * int duration = Toast.LENGTH_SHORT;
+				 * 
+				 * Toast toast = Toast.makeText(context, text, duration);
+				 * 
+				 * toast.show();
+				 
+
+				Intent intent = new Intent(ListBusStopActivity.this,
+						MainActivity.class);
+				intent.putExtra("stop_name", stop);
+				startActivity(intent);
+
+			}
+
+		});*/
+
+		/*inputSearch.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void onTextChanged(CharSequence cs, int arg1, int arg2,
+					int arg3) {
+				// When user changed the Text
+				HistoryActivity.this.adapter.getFilter().filter(cs);
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				// TODO Auto-generated method stub
+			}
+		});*/
+
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+}
--- src/com/example/barcodescanningapp/MainActivity.java
+++ src/com/example/barcodescanningapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.barcodescanningapp;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.app.ActionBar;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.loopj.android.http.AsyncHttpClient;
@@ -29,6 +36,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	private Button scanBtn;
 	private TextView formatTxt, contentTxt;
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,6 +46,9 @@ public class MainActivity extends Activity implements OnClickListener {
 		scanBtn = (Button)findViewById(R.id.scan_button);
 		formatTxt = (TextView)findViewById(R.id.scan_format);
 		contentTxt = (TextView)findViewById(R.id.scan_content);
+		ActionBar actionBar = getActionBar();
+	    actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setBackgroundDrawable(new ColorDrawable(Color.GRAY));
 
 		//listen for clicks
 		scanBtn.setOnClickListener(this);
@@ -52,6 +63,8 @@ public class MainActivity extends Activity implements OnClickListener {
 			scanIntegrator.initiateScan();
 		}
 	}
+	
+    
 
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		//retrieve result of scanning - instantiate ZXing object
@@ -89,5 +102,47 @@ public class MainActivity extends Activity implements OnClickListener {
 			toast.show();
 		}
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.activity_main_menu, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.student_history:
+	        	/*Toast toast = Toast.makeText(getApplicationContext(), 
+						"No scan data received!", Toast.LENGTH_SHORT);
+				toast.show();*/
+	        	/*Intent intent = new Intent(getApplicationContext(), HistoryActivity.class);
+	            String message = contentTxt.getText().toString();
+	            intent.putExtra(message, false);
+	            startActivity(intent);*/
+	        	try {
+	        		Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+		        	  intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		        	  startActivity(intent);
+				} catch (Exception e) {
+					// TODO: handle exception
+					System.out.println(e);
+				}
+	        	
+	        	  return true;
+	        
+	        default:
+	            return super.onOptionsItemSelected(item);
+
+	    }
+
+	}
+	
+	
+	
 
 }
